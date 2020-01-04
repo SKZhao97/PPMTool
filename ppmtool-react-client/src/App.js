10d30cea5a31512e407b6d58a9db9aa8f85aa99c
Sikai Zhao <sikaizhao1997@gmail.com>
2020-01-04 15:00:03 -0500

    Update operation

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -8,6 +8,7 @@ import AddProject from './components/Project/AddProject';
 import {Provider} from "react-redux";
 import store from "./store";
 import { render } from '@testing-library/react';
+import UpdateProject from './components/Project/UpdateProject';
 
 class App extends Component {
   render() {
@@ -18,6 +19,7 @@ class App extends Component {
             <Header />
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/updateProject/:id" component={UpdateProject} />
           </div>
         </Router>
       </Provider>
--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import {GET_ERRORS, GET_PROJECTS} from "./types";
+import {GET_ERRORS, GET_PROJECTS, GET_PROJECT} from "./types";
 import { bindActionCreators } from "redux";
 
 
@@ -7,6 +7,10 @@ export const createProject = (project, history) => async dispatch => {
     try {
         const res = await axios.post("http://localhost:8080/api/project",project);
         history.push("/dashboard")
+        dispatch({
+            type:GET_ERRORS,
+            payload:{}
+        })
     } catch (err) {
         dispatch({
             type:GET_ERRORS,
@@ -21,4 +25,16 @@ export const getProjects = () => async dispatch => {
         type: GET_PROJECTS,
         payload: res.data
     });
+};
+
+export const getProject = (id, history) => async dispatch => {
+    try {
+        const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+        dispatch({
+            type: GET_PROJECT,
+            payload: res.data
+        });
+    } catch(error) {
+        history.push("/dashboard");
+    }
 };
