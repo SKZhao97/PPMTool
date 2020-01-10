f51dcc2b231f41ca54359cdab1aa0984bec75139
Sikai Zhao <sikaizhao1997@gmail.com>
2020-01-09 20:08:56 -0500

    Backlog and ProjectTask added and the relationships set up

--- PPMToolFullStack/src/main/java/io/fullstack/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/fullstack/ppmtool/services/ProjectService.java
@@ -1,7 +1,9 @@
 package io.fullstack.ppmtool.services;
 
+import io.fullstack.ppmtool.domain.Backlog;
 import io.fullstack.ppmtool.domain.Project;
 import io.fullstack.ppmtool.exceptions.ProjectIdException;
+import io.fullstack.ppmtool.repository.BacklogRepository;
 import io.fullstack.ppmtool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,11 +13,27 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOrUpdateProject(Project project) {
 
         try {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if (project.getId() == null) {
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
+            if (project.getId() != null) {
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
             return projectRepository.save(project);
+
+
         } catch(Exception e){
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists" );
         }
